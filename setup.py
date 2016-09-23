cf55f541977ec115ef3c3c22d3927c0ea2538300
jrhoads <joseph_rhoads@brown.edu>
2016-09-23 12:26:21 -0400

    Add precomiled versions of bulstyle.css and bulstyle-blacklight.css. Update README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-bulstyle',
-    version='1.3a1',
+    version='1.4a1',
     description='Theme for Brown University Library Django Projects',
     packages=['bulstyle'],
     zip_safe=False,
