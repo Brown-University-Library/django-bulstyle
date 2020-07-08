72353287c3bfeaccad0bf75583c73ef5c4cc3831
jrhoads <joseph_rhoads@brown.edu>
2020-07-07 21:31:18 -0400

    Add templatetag and update base.html

--- bulstyle/app_settings.py
+++ bulstyle/app_settings.py
@@ -4,6 +4,10 @@ from django.conf import settings
 
 BULSTYLE_DEFAULTS = {
         'container_class': 'container',
+        'header_url': 'https://library.brown.edu/common/includes/bul_pl_header.php',
+        'header_cache_length': 500,
+        'header_subnav_key': '',
+        'header_subnav_login_key': '',
 }
 
 BULSTYLE = BULSTYLE_DEFAULTS.copy()
@@ -18,3 +22,15 @@ def get_bulstyle_setting(setting, default=None):
 
 def container_class():
     return get_bulstyle_setting('container_class')
+
+def header_url():
+    return get_bulstyle_setting('header_url')
+
+def header_cache_length():
+    return get_bulstyle_setting('header_cache_length')
+
+def header_subnav_key():
+    return get_bulstyle_setting('header_subnav_key')
+
+def header_subnav_login_key():
+    return get_bulstyle_setting('header_subnav_login_key')
