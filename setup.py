26dfec878f1a7b6d82947cdbcea88a0b7143e7ba
Joseph Rhoads <joseph_rhoads@brown.edu>
2021-04-07 15:10:50 -0400

    Bump Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-bulstyle',
-    version='1.4a',
+    version='1.5',
     description='Theme for Brown University Library Django Projects',
     packages=['bulstyle'],
     zip_safe=False,
