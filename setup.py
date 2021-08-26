29d2b8a42a6d7008cd72970e58a29a7651db0866
Joseph Rhoads <joseph_rhoads@brown.edu>
2021-08-26 09:19:49 -0400

    Update setup.py
    Finding subdirectories and add them to the package.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='django-bulstyle',
-    version='1.6',
+    version='1.7',
     description='Theme for Brown University Library Django Projects',
-    packages=['bulstyle'],
+    packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
     install_requires=[
